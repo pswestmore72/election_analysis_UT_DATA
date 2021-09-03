48acf6ab6e035187ec2b96aee2fe0047a02e51b6
unknown <pswestmore72@gmail.com>
2021-09-03 13:55:11 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -2,8 +2,8 @@
 import datetime as dt
 import csv, random, os # numpy
 
-now = dt.datetime.now()
-print("The time right now is ", now)
+# now = dt.datetime.now()
+# print("The time right now is ", now)
 
 file_path_read = os.path.join("resources", "election_results.csv")
 file_path_save = os.path.join("analysis", "election_analysis.txt")
@@ -24,30 +24,27 @@ with open(file_path_read) as election_data:
         else:
             candidate_opts[row[2]]['votes'] += 1
         total_votes += 1
-print(f"Total votes: {total_votes:,}")
+
 winning_percentage = 50
 winning_count = total_votes * 0.50
-for key in candidate_opts.keys():
-    candidate_opts[key]['vote_percentage'] = (float(candidate_opts[key]['votes']) / float(total_votes)) * 100
-    print(f"{candidate_opts[key]['name']}: received {candidate_opts[key]['vote_percentage']:.1f}% ({candidate_opts[key]['votes']:,})")
-    if candidate_opts[key]['vote_percentage'] > winning_percentage and candidate_opts[key]['votes'] > winning_count:
-        candidate_opts[key]['winner'] = True
-        winning_candidate = candidate_opts[key]['name']
-    else:
-        candidate_opts[key]['winner'] = False
-    
-print(
-    f"-------------------------\n"
-    f"Winner: {candidate_opts[winning_candidate]['name']}\n"
-    f"Winning Vote Count: {candidate_opts[winning_candidate]['votes']:,}\n"
-    f"Winning Percentage: {candidate_opts[winning_candidate]['vote_percentage']:.1f}%\n"
-    f"-------------------------\n")
-
 
 # Using the with statement open the file as a text file.
 with open(file_path_save, "w") as analysis_file:
-    analysis_file.write('Election Results\n\n')
-    analysis_file.write('Counties  in the Election\n--------------------\nArapahoe\nDenver\nJefferson')
+    analysis_file.write(f"Election Results\n-------------------------\n")
+    analysis_file.write(f"Total votes: {total_votes:,}\n-------------------------\n")
+
+    for key in candidate_opts.keys():
+        candidate_opts[key]['vote_percentage'] = (float(candidate_opts[key]['votes']) / float(total_votes)) * 100
+        analysis_file.write(f"{candidate_opts[key]['name']}: received {candidate_opts[key]['vote_percentage']:.1f}% ({candidate_opts[key]['votes']:,})\n")
+        if candidate_opts[key]['vote_percentage'] > winning_percentage and candidate_opts[key]['votes'] > winning_count:
+            candidate_opts[key]['winner'] = True
+            winning_candidate = candidate_opts[key]['name']
+        else:
+            candidate_opts[key]['winner'] = False
+    analysis_file.write("-------------------------\n")
+    analysis_file.write(f"Winner: {candidate_opts[winning_candidate]['name']}\n")
+    analysis_file.write(f"Winning Vote Count: {candidate_opts[winning_candidate]['votes']:,}\n")
+    analysis_file.write(f"Winning Percentage: {candidate_opts[winning_candidate]['vote_percentage']:.1f}%\n-------------------------\n")
 
 # Close the file
 analysis_file.close()
